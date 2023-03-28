8dc1e3e06a3e6cd41a89460b0d3c61f1fe00ef47
luislopezg88 <luislopezg88@gmail.com>
2023-03-28 17:34:20 -0400

    User

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -1,5 +1,6 @@
 const models = require("../models/index");
 const User = models.User;
+const bcrypt = require('bcryptjs');
 
 exports.findById = async (req, res) => {
   try {
@@ -12,13 +13,27 @@ exports.findById = async (req, res) => {
   }
 };
 
+exports.findAll = async (req, res) => {
+  try {
+    const rows = await User.findAll({
+      include: [
+        {model: models.Room },
+      ],
+    });
+    res.send(rows);
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ message: "Error de Servidor", success: false });
+  }
+};
+
 exports.create = async(req, res) => {
-    console.log("entra en el create user controller");
+    
     try {
       const row = await User.create({
             userName: req.body.userName,
             email: req.body.email,
-            password: req.body.password,
+            password: bcrypt.hashSync(req.body.password, 10) ,
             status: req.body.status
       });
       res.send(row); 
@@ -26,6 +41,38 @@ exports.create = async(req, res) => {
       console.log(error)
       res.status(400).json({ message: "Error de Servidor", success: false });
     }
+};
+
+
+exports.update = async(req, res) => {
+  const id = req.params.id;
+  try {
+    await User.update({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10) ,
+      status: req.body.status
+    },
+    {
+      where: { id },
+    })
+    res.status(200).send("updated successfully a User with id = " + id);
+  } catch (error) {
+    res.status(400).json({ message: "Error de Servidor", success: false });
+  }
+};
+
+
+exports.delete = async(req, res) => {
+  const id = req.params.id;
+  try {
+    await User.destroy({
+      where: { id },
+    });
+    res.status(200).send("deleted successfully a User with id = " + id);
+  } catch (error) {
+    res.status(400).json({ message: "Error de Servidor", success: false });
+  }
 };
 
 
